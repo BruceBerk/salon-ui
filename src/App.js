c700a63c35375f9c6198005c93b23d5d81960ebe
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2020-08-06 22:18:17 -0400

    Got the navbar text to display white

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Navbar from 'react-bootstrap/Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark">
+      <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">AR Salon & Day Spa</Navbar.Brand>
       </Navbar>
-      <div class="text-center">
+      <div className="center">
         <h2>
           Edit <code>src/App.js</code> and save to reload.
         </h2>
